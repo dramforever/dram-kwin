e56c4d22eee2b36bd57eeb9d817d26828134a1c1
dramforever <dramforever@live.com>
2022-02-19 22:39:31 +0800

    Handle menus in Telegram

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,12 +1,15 @@
 // Minimize Telegram window when unfocused
+
+const telegramWindows = [];
+
 function addTelegramWatcher(client) {
-    if (client.resourceClass.toString() !== 'telegramdesktop') {
-        return;
+    if (client.resourceClass.toString() === 'telegramdesktop') {
+        telegramWindows.push(client);
     }
 
     client.activeChanged.connect(() => {
-        if (! client.active) {
-            client.minimized = true;
+        if (client.active && client.resourceClass.toString() !== 'telegramdesktop') {
+            telegramWindows.forEach((w) => { w.minimized = true; });
         }
     });
 }
@@ -14,6 +17,12 @@ function addTelegramWatcher(client) {
 function main() {
     workspace.clientList().forEach(addTelegramWatcher);
     workspace.clientAdded.connect(addTelegramWatcher);
+    workspace.clientRemoved.connect((client) => {
+        const i = telegramWindows.indexOf(client);
+        if (i >= 0) {
+            telegramWindows.splice(i, 1);
+        }
+    });
 }
 
 main();
